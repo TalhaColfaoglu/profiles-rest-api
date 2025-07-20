0aaded6a163ca7bb09fcca9f9be02f35048c2727
Talha Colfaoğlu <colfaoglutalha@gmail.com>
2025-07-20 12:32:01 +0300

    Added hello api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView #➡️ DRF’in temel APIView sınıfını içeri aktarır. Bu, HTTP isteklerini (GET, POST, vs.) işleyebilmeni sağlar.
+from rest_framework.response import Response #➡️ API'den JSON formatında veri döndürmek için kullanılır. Django'nun HttpResponse'una benzer ama DRF'e özel.
 
-# Create your views here.
+class HelloApiView(APIView): #➡️ APIView'den türeyen özel bir sınıf tanımladın. Bu senin kendi API endpoint’in olacak. Yani bu sınıf, bir URL adresine bağlanıp cevap verecek.
+    """Test API View"""
+
+    def get(self, request, format=None): #➡️ Bu metod, biri tarayıcıdan ya da Postman'den GET isteği gönderdiğinde çalışır. format=None demek, örneğin .json uzantısı alabilir ama zorunlu değil.
+        """Returns a list of APIView features"""
+        an_apiview = [ #➡️ Bu, APIView'in ne işe yaradığını açıklayan bir liste. Öğretici amaçlı yazılmış.
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application logic',
+            'Is mapped manually to URLs'
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('admin/', admin.site.urls), 
+    path('api/', include('profiles_api.urls')) #`api/`: Kullanıcı tarayıcıda ya da uygulamada `/api/...` diye başlayan bir URL'e giderse bu çalışır.
+]   #“Bu `api/` ile başlayan tüm URL’ler, `profiles_api` uygulamasının içindeki `urls.py` dosyasına yönlendirilecek.”
